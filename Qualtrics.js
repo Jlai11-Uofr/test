6e1ab7ed992c2ac2c246108b591e8add87cb16e0
Jlai11-Uofr <61286293+Jlai11-Uofr@users.noreply.github.com>
2020-08-12 18:04:31 -0400

    Update Qualtrics.js

--- Qualtrics.js
+++ Qualtrics.js
@@ -6,7 +6,7 @@ var timeline = [];
 //                ******************Helper Function in python 3.7 that will help do stuff*
 /* import os
 
-directory = "C:\\Users\\Jonathan\\Desktop\\Js\\stimuli\\stimuli"
+directory = "C:\\Users\\Jonathan\\Desktop\\Js\\stimuli"
 
 
 empty_arary = []
@@ -33,7 +33,7 @@ html:'<p style="color:red;"> ID <input name="id" type="text" />  </p>'
 
 
 timeline.push(form_trial);
-var source1 =  "stimuli/stimuli/";
+var source1 =  "stimuli/";
 var x = ['Noise.wav', 'Sound10c.wav', 'Sound10s.wav', 'Sound11c.wav', 'Sound11s.wav', 'Sound12c.wav', 'Sound12s.wav', 'Sound13c.wav', 'Sound13s.wav', 'Sound14c.wav', 'Sound14s.wav', 'Sound15c.wav', 'Sound15s.wav', 'Sound16c.wav', 'Sound16s.wav', 'Sound17c.wav', 'Sound17s.wav', 'Sound18c.wav', 'Sound18s.wav','Sound19c.wav','Sound19s.wav', 'Sound1c.wav', 'Sound1s.wav', 'Sound20c.wav', 'Sound20s.wav', 'Sound21c.wav', 'Sound21s.wav', 'Sound22c.wav', 'Sound22s.wav', 'Sound23c.wav', 'Sound23s.wav', 'Sound24c.wav', 'Sound24s.wav', 'Sound25c.wav', 'Sound25s.wav', 'Sound2c.wav', 'Sound2s.wav', 'Sound3c.wav', 'Sound3s.wav', 'Sound4c.wav', 'Sound4s.wav', 'Sound5c.wav', 'Sound5s.wav', 'Sound6c.wav', 'Sound6s.wav', 'Sound7c.wav', 'Sound7s.wav', 'Sound8c.wav', 'Sound8s.wav', 'Sound9c.wav', 'Sound9s.wav']
 
 var x =   shuffle(x)
